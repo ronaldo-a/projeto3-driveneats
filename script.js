5f6b4d0aa81508dbbb762bceb843f5aa946f14b1
Ronaldo <ronaldoandrade966@gmail.com>
2022-06-10 16:21:32 -0300

    inicio do JS

---
+++ script.js
@@ -0,0 +1,17 @@
+function selector() {
+  let cartao = document.querySelector('.prato .card')
+
+  cartao.classList.add('selected')
+}
+
+function selector1() {
+  let cartao = document.querySelector('.bebida .card')
+
+  cartao.classList.add('selected')
+}
+
+function selector2() {
+  let cartao = document.querySelector('.sobremesa .card')
+
+  cartao.classList.add('selected')
+}
