745cf7df16d84609e480264445cd60bdd6373e72
Ronaldo <ronaldoandrade966@gmail.com>
2022-06-12 11:00:05 -0300

    msg semi pronta

--- script.js
+++ script.js
@@ -49,3 +49,33 @@ function fecharPedido() {
     document.querySelector('.botao').classList.add('ativo')
   }
 }
+
+function enviarPedido() {
+  let prato = document.querySelector('.prato .selected h4').innerHTML
+  let bebida = document.querySelector('.bebida .selected h4').innerHTML
+  let sobremesa = document.querySelector('.sobremesa .selected h4').innerHTML
+
+  let pratoPreco = document.querySelector('.prato .selected .preco').innerHTML
+  let bebidaPreco = document.querySelector('.bebida .selected .preco').innerHTML
+  let sobremesaPreco = document.querySelector(
+    '.sobremesa .selected .preco'
+  ).innerHTML
+
+  let pratoPrecoNum = pratoPreco.replace('R$', '')
+  pratoPrecoNum = Number(pratoPrecoNum.replace(',', '.'))
+  let bebidaPrecoNum = bebidaPreco.replace('R$', '')
+  bebidaPrecoNum = Number(bebidaPrecoNum.replace(',', '.'))
+  let sobremesaPrecoNum = sobremesaPreco.replace('R$', '')
+  sobremesaPrecoNum = Number(sobremesaPrecoNum.replace(',', '.'))
+  let preco = pratoPrecoNum + bebidaPrecoNum + sobremesaPrecoNum
+  preco = preco.toFixed(2)
+
+  let text = `Olá, gostaria de fazer o pedido:
+  - Prato: ${prato}
+  - Bebida: ${bebida}
+  - Sobremesa: ${sobremesa}
+Total: R$ ${preco}`
+
+  let msg = encodeURIComponent(text)
+  console.log(msg)
+}
